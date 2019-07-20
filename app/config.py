b535dd359dd7a9368795ae69d38774c64f104c9a
Cyndi Lopez <cyndilopez6@gmail.com>
2019-07-20 11:02:44 -0700

    hid access key

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
 import os
 
-# S3_BUCKET = os.environ.get("S3_BUCKET")
-# S3_KEY = os.environ.get("S3_KEY")
-# S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+S3_BUCKET = os.environ.get("S3_BUCKET")
+S3_KEY = os.environ.get("S3_KEY")
+S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
 
+# S3_BUCKET = 'lpr-cyndi'
+# S3_KEY = 'AKIAJNZGM3HKE647GWMQ'
+# S3_SECRET = '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'
