f7977e493e369fbb16c576e57103474a8dfdcc5b
Cyndi Lopez <cyndilopez6@gmail.com>
2019-07-20 10:20:42 -0700

    initial commit

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+
+# S3_BUCKET = os.environ.get("S3_BUCKET")
+# S3_KEY = os.environ.get("S3_KEY")
+# S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+
+S3_BUCKET = 'lpr-cyndi'
+S3_KEY = 'AKIAIS7A7B27EGNZVSPQ'
+S3_SECRET = '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'
