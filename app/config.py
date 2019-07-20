ac2a3d1b24f36ddccd6afc27ff3d73cc91042fd5
Cyndi Lopez <cyndilopez6@gmail.com>
2019-07-20 10:28:53 -0700

    deleted keys

--- app/config.py
+++ app/config.py
@@ -4,6 +4,3 @@ import os
 # S3_KEY = os.environ.get("S3_KEY")
 # S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
 
-S3_BUCKET = 'lpr-cyndi'
-S3_KEY = 'AKIAIS7A7B27EGNZVSPQ'
-S3_SECRET = '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'
